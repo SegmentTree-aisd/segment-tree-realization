7d1671ea5144219755a44b08aaacb1f0e901cbe9
Regina <rtapkina@gmail.com>
2021-05-16 17:07:50 +0300

    fix conflicts

--- src/tree/SegmentTree.h
+++ src/tree/SegmentTree.h
@@ -1,9 +1,11 @@
 #ifndef SEGMENTTREE_SEGMENTTREE_H
 #define SEGMENTTREE_SEGMENTTREE_H
+using namespace std;
 
 void build(std::vector<long long> &tree, const std::vector<long long> &arr, int v, int tl, int tr);
 int rsq(std::vector<long long> &tree, int v, int tl, int tr, int ql, int qr);
 void update(std::vector<long long> &tree, int v, int tl, int tr, int pos, int new_val);
 void build_max(std::vector<pair<long long, int>> &tree,std::vector<long long> &arr, int v, int tl, int tr);
 pair<long long, int> getmax(std::vector<pair<long long, int>> &tree, int v, int tl, int tr, int ql, int qr);
+
 #endif //SEGMENTTREE_SEGMENTTREE_H
